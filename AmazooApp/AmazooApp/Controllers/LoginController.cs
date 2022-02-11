5cf43edad6ccc5750398e2e6319fff7748e64313
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2022-02-11 00:42:20 -0500

    Feature: Login (Reference to the login task #14)   - Created the login Branch and then pulled from the Beta to test run the project in local   - Created A template view for the login page   - Created A template controller for the login page   - Added a T-sql table of users to test database link on azure

---
+++ AmazooApp/AmazooApp/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AmazooApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
