73ec0093ec7dcdf86921afccada0ca0dd1881f58
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2022-02-13 21:02:01 -0500

    Implemented account button to output additional links #53

--- AmazooApp/AmazooApp/wwwroot/js/site.js
+++ AmazooApp/AmazooApp/wwwroot/js/site.js
@@ -85,4 +85,57 @@ function hideMenuAdmin() {
     var container = document.getElementById("menu");
     container.parentNode.removeChild(container);
 }
+
+function showAccount() {
+    if (document.getElementById("menu")) {
+        hideMenuAdmin();
+    }
+    var account_but = document.getElementById("account_but")
+    account_but.onclick = hideAccount;
+
+    var parent = document.getElementById("container");
+
+    var container = document.createElement("div");
+    container.id = "acc_menu";
+    container.style.width = "fit-content";
+    container.style.height = "fit-content";
+    container.style.position = "absolute";
+    container.style.right = "0px";
+    container.style.backgroundColor = "black";
+
+    var hiUser = document.createElement("p");
+    var logout = document.createElement("a");
+    var myAccount = document.createElement("a");
+    var myOrders = document.createElement("a");
+
+    hiUser.style.display = "block";
+    hiUser.style.color = "white";
+    hiUser.style.fontSize = "calc(18px + 0.5vw)";
+    hiUser.textContent = "Hi, " + "<User's Name>"; //Implement user's name detection
+
+    myAccount.className = "menu_account_item";
+    myAccount.textContent = "My Account";
+    myAccount.style.margin = "10px 0 0 0";
+
+    myOrders.className = "menu_account_item";
+    myOrders.textContent = "My Orders";
+
+    logout.className = "menu_account_item";
+    logout.textContent = "Logout";
+
+    container.appendChild(hiUser);
+    container.appendChild(myAccount);
+    container.appendChild(myOrders);
+    container.appendChild(logout);
+
+    parent.appendChild(container);
+}
+
+function hideAccount() {
+    var account_but = document.getElementById("account_but")
+    account_but.onclick = showAccount;
+
+    var container = document.getElementById("acc_menu");
+    container.parentNode.removeChild(container);
+}
 //NAVIGATION BAR FUNCTIONS END
