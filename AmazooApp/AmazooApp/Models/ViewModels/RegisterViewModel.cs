d86954e81a454efb7ce871c4bad4c7b00aef77f0
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2022-02-14 01:15:35 -0500

    Feature Sign-up #63 - Added The necessary tables for creating a new user - Added the controller for the registartion page - Added the view with minimum styling - Synced with the login branch for shared content - Implemented role assignments and related features

---
+++ AmazooApp/AmazooApp/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmazooApp.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [StringLength(100,ErrorMessage ="the {0} must atleast {2} x-acteers long",MinimumLength =6)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name ="Role")]
+        public string Role { get; set; }
+
+    }
+}
---
+++ AmazooApp/AmazooApp/Utility/RoleHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace AmazooApp.Utility
+{
+    public class RoleHelper
+    {
+        public static string Admin = "Admin";
+        public static string Customer = "Customer";
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value = Admin , Text=RoleHelper.Admin},
+                new SelectListItem{Value=Customer , Text=RoleHelper.Customer}
+            };
+        }
+    }
+}
