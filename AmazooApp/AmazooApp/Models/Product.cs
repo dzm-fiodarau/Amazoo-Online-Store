9c661d0ef97466538a9504c348eb76df21c32b79
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2022-02-14 18:28:53 -0500

    Output all products from the database Products table #68

--- AmazooApp/AmazooApp/Models/Product.cs
+++ AmazooApp/AmazooApp/Models/Product.cs
@@ -11,18 +11,18 @@ namespace AmazooApp.Models
         [Key]
         public int Id { get; set; }
 
-        public int ProductName { get; set; }
+        public string ProductName { get; set; }
 
-        public int Description { get; set; }
+        public string Description { get; set; }
 
         public int QuantityInStock { get; set; }
 
-        public int Price { get; set; }
+        public float Price { get; set; }
 
-        public int Category { get; set; }
+        public string Category { get; set; }
 
-        public int Image { get; set; }
+        public string Image { get; set; }
 
-        public int Brand { get; set; }
+        public string Brand { get; set; }
     }
 }
