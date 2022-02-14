e285f95bbcc65b4d00b6e7851c1e80d7cd616d18
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2022-02-14 16:32:06 -0500

    Modified and Styled The login page #70

--- AmazooApp/AmazooApp/Controllers/LoginController.cs
+++ AmazooApp/AmazooApp/Controllers/LoginController.cs
@@ -29,44 +29,34 @@ namespace AmazooApp.Controllers
 
         public IActionResult Check()
         {
-            return View();
+            return Ok("WE can Login YAY!");
         }
 
 
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Check(LoginViewModel userProfileModel)
+        public async Task<IActionResult> Check(LoginViewModel model)
         {
-
+            //TODO ADD [Compare("Password",ErrorMessage ="Password and confirmation password not match.")]
             if (ModelState.IsValid)
             {
-                //TODO Add the remember checkbox
-                var result = await _signInManager.PasswordSignInAsync(userProfileModel.Email, userProfileModel.Password, true,false);
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,false);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index", "Home");
                 }
-                ModelState.AddModelError("Login", "Invalid Login Attempt");
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
             }
-            return View(userProfileModel);
+            return View("Login",model);
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Login");
         }
 
-        public IActionResult Register()
-        {
-            return View();
-        }
 
-        private ApplicationUser createUser()
-        {
-            return new ApplicationUser
-            {
-                UserName = "John",
-                Email = "a@a",
-                FirstName = "John",
-                LastName = "Doe",
-                PasswordHash = "123"
-            };
-        }
     }
 }
