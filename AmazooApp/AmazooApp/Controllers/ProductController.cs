7fdcd4fc47192c7a44d10459905fcf432439dfb7
Cosmin Suna <c_suna@live.concordia.ca>
2022-02-17 20:14:59 -0500

    Added the option to add and delete products from website. But the image display for these still needs to be worked on.

--- AmazooApp/AmazooApp/Controllers/ProductController.cs
+++ AmazooApp/AmazooApp/Controllers/ProductController.cs
@@ -23,9 +23,59 @@ namespace AmazooApp.Controllers
             return View(productList);
         }
 
+        //GET-Add
         public IActionResult AddProduct()
         {
             return View();
         }
+
+        //POST-Add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddProduct(Product obj)
+        {
+            _db.Products.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        //GET-Delete
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Products.Find(id);
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            
+            return View(obj);
+        }
+
+        //POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Products.Find(id);
+         if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Products.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        
+        }
+
+
+
     }
 }
