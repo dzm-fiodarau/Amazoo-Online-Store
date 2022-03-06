7ffa4bbb82c1c8cd471f4746581518df690691ab
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2022-03-06 16:23:12 -0500

    Removed the products list and added authentication #110

--- AmazooApp/AmazooApp/Controllers/BillingController.cs
+++ AmazooApp/AmazooApp/Controllers/BillingController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AmazooApp.Utility;
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace AmazooApp.Controllers
 {
+    [Authorize]
     public class BillingController : Controller
     {
-       
+       [Authorize]
         public IActionResult Billing()
         {
             return View();
