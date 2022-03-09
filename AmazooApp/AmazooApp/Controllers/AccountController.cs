2fba1766b00f09ddadf220fcf4f91c589da3c631
Dzmitry Fiodarau <dzm.fiodarau@gmail.com>
2022-03-08 19:10:01 -0500

    Created My Account page with empty table #121

---
+++ AmazooApp/AmazooApp/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using AmazooApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazooApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserManager<ApplicationUser> _userManager;
+
+        public IActionResult MyAccount()
+        {
+
+            return View();
+        }
+    }
+}
