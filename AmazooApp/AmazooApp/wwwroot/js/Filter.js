d12e3af7b7bb895618717586dcdbd091d083c46f
Kevin Rwigamba <k_rwigam@live.concordia.ca>
2022-03-11 17:40:04 -0500

    Added the code to handle checkbox persistance #131

---
+++ AmazooApp/AmazooApp/wwwroot/js/Filter.js
@@ -0,0 +1,24 @@
+﻿
+//window.addEventListener('load', load)
+
+$(document).ready(function () {
+    
+    const Save = () => {
+        localStorage.clear();
+        var checkbox = document.getElementsByName('chckBox');
+        for (let i = 0; i < checkbox.length; i++) {
+            console.log("The checkbox value is ", checkbox[i]);
+            localStorage.setItem(checkbox[i].id, checkbox[i].checked);
+        }
+    }
+
+    const load = () => {
+        var checkbox = document.getElementsByName('chckBox');
+        for (let i = 0; i < checkbox.length; i++) {
+            var checked = JSON.parse(localStorage.getItem(checkbox[i]));
+            console.log("The checkbox value is ", checked);
+            document.getElementById(checkbox[i].id).checked = checked;
+        }
+
+    }
+});
