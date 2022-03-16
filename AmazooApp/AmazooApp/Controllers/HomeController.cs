ebc6e310b1328a7870bb8961cd5b79e4ac8f66ab
elion-abdyli <elion.3@live.com>
2022-03-16 13:46:54 -0400

    removed old filter implementation as per #20 and #142

--- AmazooApp/AmazooApp/Controllers/HomeController.cs
+++ AmazooApp/AmazooApp/Controllers/HomeController.cs
@@ -25,13 +25,10 @@ namespace AmazooApp.Controllers
                        select p;
         }
 
-        public async Task<IActionResult> Index(String searchEntry, String b1, String b2, String b3, String b4)
+        public async Task<IActionResult> Index(String searchEntry)
         {
             //Console.WriteLine("HELLO");
-            //Console.WriteLine(b1==null);
-            //Console.WriteLine(b2 == null);
-            //Console.WriteLine(b3 == null);
-            //Console.WriteLine(b4 == null);
+
 
             ViewBag.SearchEntry = searchEntry;
 
@@ -40,25 +37,6 @@ namespace AmazooApp.Controllers
             if (!String.IsNullOrEmpty(searchEntry))
             {
                 products = products.Where(p => p.ProductName.Contains(searchEntry) || p.Brand.Contains(searchEntry));
-            }
-            if (!(b1 == null)){
-                Console.WriteLine(b1);
-                products = products.Where(p => p.Category.Contains(b1));
-            }
-            if (!(b2 == null))
-            {
-                Console.WriteLine(b2);
-                products = products.Where(p => p.Category.Contains(b2));
-            }
-            if (!(b3 == null))
-            {
-                Console.WriteLine(b3);
-                products = products.Where(p => p.Category.Contains(b3));
-            }
-            if (!(b4 == null))
-            {
-                Console.WriteLine(b4);
-                products = products.Where(p => p.QuantityInStock>0);
             }
 
             return View(await products.ToListAsync());
